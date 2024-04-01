d65eda4b22077141db7653e43a14d4c1d2e98457
FreakRacer <bggy@MacBook-Air-kisabup.local>
2024-04-01 08:58:19 +0300

    первый commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// mission: javaScript
+
+let a = 2;
+let b = 5;
+
+let 1a = "error"
+let -b = "error"
+   
+b = a + b
+b = a - b
+a = a - b
+
+console.log(a);
+
+let name = "John";
+let admin = name;
+
+alert(admin)
+
+
+console.log(a);
