ffcd6d4aee61e65f9b9ef9f0e8f4bc1acfd7a58a
FreakRacer <bggy@MacBook-Air-kisabup.local>
2024-04-03 09:15:53 +0300

    add new block and fix main-block and letter spaces

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// mission: javaScript
-
+/*
 let a = 2;
 let b = 5;
 
@@ -10,7 +9,7 @@ b = a + b
 b = a - b
 a = a - b
 
-console.log(a);
+//console.log(a);
 
 let name = "John";
 let admin = name;
@@ -18,4 +17,42 @@ let admin = name;
 alert(admin)
 
 
-console.log(a);
+//console.log(a); */
+
+//mission ; JavaScript 2
+
+
+/*   let date: number = 28.02.1999;
+let P: BigInt = 3.14: 
+
+let a = 2;
+
+const b = 2;
+
+// b = 3;
+
+console.log(b)
+ 
+let ruble = 100 
+let dollar = 1
+
+alert(' ruble =  ' + ruble + '   dollar =   ' + dollar)
+
+let i_have = prompt ('i have rubles') 
+let i_give = i_have / ruble
+
+alert(i_give)
+
+let name = prompt ('your name?')
+
+let adress = prompt('your adress?')
+
+let phone = prompt('your phone?')
+
+console.log(    
+    `student name: ${name}; 
+    student adress: ${adress};
+    student phone: ${phone} `
+)
+*/
+
