4e79bfae8f1434311fdd170820b4d539f1fca261
FreakRacer <bggy@MacBook-Air-kisabup.local>
2024-04-14 22:38:17 +0300

    add last block "footer" and add cur. pointer on logos and try complete homework

---
+++ js/ab.js
@@ -0,0 +1,75 @@
+
+//1
+let name = 'imam'; //string
+
+//2
+const age = 18; //num
+
+//3
+if (age < 15) {
+    console.log(`${age} < 15`);
+    
+} else{
+    console.log(`hello! ${name}`);
+};
+
+//4
+function viewMoney(money) {
+    console.log("give 10$");
+    
+};
+viewMoney();
+
+//5
+function getMoney(money){
+    return ("give 10$");
+};
+getMoney(); 
+
+//6
+function cash(cash) {
+    return (` take your ${cash} $`);
+    
+};
+cash('10$'); 
+
+//7
+function box (num) {
+    console.log(num * num);
+    
+};
+box(10);
+
+//8
+function max(a, b, c) {
+    if (a > b > c) {
+        console.log(a);
+    } else if( b > a > c){
+        console.log(b);
+    }
+    else{
+        console.log(c);
+    }
+    
+};
+
+max(1, 2, 3);
+
+//9 skip
+
+//10
+function just(a, b) {
+
+    if( a%2==0 && b%2==0){
+        return a * b
+
+        }else if( a%2==!0 && b%2==!0){
+            return a + b
+        } else if ( a%2==!0 && b%2==0){  
+            return a
+            } else{
+                return b
+            }};
+just(2, 3 );
+
+
