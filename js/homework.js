e681a50fc6499f4aab6ed0130b9ff90b552e681b
FreakRacer <bggy@MacBook-Air-kisabup.local>
2024-04-22 18:14:12 +0300

    add adapt 768px and homework

--- js/homework.js
+++ js/homework.js
@@ -221,7 +221,7 @@ while (num < 3) {
 
 //  1
 
-
+/*
 let a = ["lorem", 123, true, "ipsum"]
 
 console.log(a.length);
@@ -258,5 +258,187 @@ let b = n.map(
     //if (b % 2 == 0) continue;
  //    console.log(b);
 
+*/
 
 
+// misiion js : 7
+
+/*
+let mas = [1, 2];
+
+let mas2 = ['lorem', 2, true,  898,]
+
+let Text = 'lorem ipsum text more'
+
+
+const data = 
+[
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    }
+]
+ */
+/*
+let mas3 = mas.reduce(function(acc, item, idex, Array){
+     return item + acc
+}, 0 )
+
+console.log(mas3)
+*/
+
+/*
+let mas3 = data.filter(function(item, index, arr){
+    if( item.id > 1){
+     return item
+    }
+}) 
+*/
+
+
+/*
+let mas3 = data.map(function(item , idex, array){
+    return item.title
+})
+
+console.log(mas3)
+*/
+
+
+/*
+let mas5 = mas.map(function(item, index, Array) {
+    //return item + 1
+    return `i can change ${item} and: ${index}`
+})
+*/
+
+
+/*
+mas2.forEach(function(item, index, Array){
+    console.log(`elem : ${item } index: ${index}`)
+
+}) */
+
+
+//let mas3 = mas.concat(mas2)
+
+//mas.reverse()
+
+//let mas3 = mas2.includes('lorem')
+
+//let mas3 = mas.indexOf(9)
+
+//let mas3 = Text.split( ' ')
+
+//let mas4 =  mas3.join(' ')
+
+
+
+/*
+
+let aaa = [10, 2762, 0.42, 192 , 3, 28, 1125, 2018, 34, 66, 100]
+
+aaa.forEach(function(item, index, array){
+    if(  item % 2 != 0){
+        console.log(item)
+    }
+});
+
+
+let ass = 'lerem text dollar me bigboss junky funky worm'
+
+let ass2 = mas.split( ' ');
+
+
+let ass3 = ass2.filter (function(item, index){
+    if( item.length < 4){
+       return item
+    }
+});
+console.log(ass3)
+
+*/
+
+/*
+let agg = [1, -3, 5, 6, -7, 8, 9, -11]
+
+let agg2 = agg.filter(function(item, index, array){
+    return item < 0
+    
+})
+console.log(agg2)
+
+let ann = [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
+
+let ann2 = ann.filter(function(item, index, array){
+    if(item % 2 == 0){
+        return item
+    }
+})
+console.log(ann2)
+
+let aff = [ "lorem",  "ipsum", "javascript", "php", "css", "react", "git", "html", "mysql"]
+
+let aff2 = aff.map(function(item, index, array){
+    if(item.length != 3){
+        console.log(item)
+    }
+})
+
+let att = [5, 6, 7, 8, 9];
+
+let att2 = att.map(function(item, index){
+    return item * item
+})
+console.log(att2)
+
+
+
+let ajj = [-13, 0, 12, 1662, -0.32, -102, -2];
+
+let ajj2 = ajj.filter(function(item, index){
+    if( item < 0){
+        return item 
+    }
+})
+let ajj3 = ajj2.reduce(function(acc,item, index){
+ 
+    return acc + item 
+
+}, 0)
+
+console.log(ajj3)
+
+let ahh = [{a : 10,
+            b : 5},
+           {a : 20, 
+            b : 22},
+           {a : 131, 
+            b : 55}];
+
+
+let ahh2 = ahh.map(function( item, index){
+   return item.a + item.b})
+console.log(ahh2)
+
+let ayy = [{ x : 10, y : 'lorem'}, { x : 21, y : 'lorem'}, { x : - 17, y : 'lorem'}, { x : 156, y : 'lorem'}];
+
+let ayy2 = ayy.reduce(function(acc, item, index){
+        return item.x + acc
+}, 0)
+console.log(ayy2);
+*/
